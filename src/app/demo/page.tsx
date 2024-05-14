2d837ffb02a04c642a7ac02c9cc0edf2152170d0
buchananwill <139961977+buchananwill@users.noreply.github.com>
2024-05-14 01:11:15 +0100

    Added a title.

--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -8,13 +8,14 @@ import JsonListenerDiv from "@/app/demo/components/JsonListenerDiv";
 
 export default function Page() {
   return (
-    <>
+    <div className={'flex-col py-8 gap-4'}>
       <ControllerComponent
         contextKey={valueContextKey}
         initialValue={initialNthValue}
       />
         <ControllerComponent contextKey={LogContent} initialValue={''}/>
-      <div className={"grid gap-2 grid-cols-5 p-16"}>
+        <h1 className={'text-lg font-semibold drop-shadow border-8 rounded-lg p-4 text-center mx-8 block mb-8'}>Selective Context Demo</h1>
+      <div className={"grid gap-2 grid-cols-5 px-16"}>
 
           <ClientFunctionWrapper>
         <PrintableListenerDiv
@@ -26,6 +27,6 @@ export default function Page() {
           </ClientFunctionWrapper>
           <JsonListenerDiv className={'border-4 col-span-5 h-fit rounded-lg p-4 min-h-8'} contextKey={LogContent} listenerKey={'read-out-div'} initialValue={''}/>
       </div>
-    </>
+    </div>
   );
 }
